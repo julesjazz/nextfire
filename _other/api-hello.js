da9e8ef977484f36afda62fa04e137817f9fb246
jSilver <julesrsilver@gmail.com>
2021-11-28 15:30:49 -0800

    init + notes

--- _other/api-hello.js
+++ _other/api-hello.js
@@ -3,7 +3,3 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
-
-// exec only on the server
-// will not add to js bundle size
-// can use to query firebase, but not for this project
